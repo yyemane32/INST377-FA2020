861b22e4ffaa3f634f3dd0ca20decd9617a730ea
Estifanos Kidane <ekidane@terpmail.umd.edu>
2020-10-05 21:13:06 -0400

    progress

---
+++ public/asst-1/styles.cs
@@ -0,0 +1,31 @@
+.header {
+  padding: 60px;
+  text-align: center;
+  background: #1abc9c;
+  color: white;
+  font-size: 30px;
+}
+.header img {
+  width: 100%;
+  height: 100px;}
+
+
+.wrapper {
+  margin-right: auto; 
+  margin-left:  auto; 
+
+  max-width: 960px; 
+
+  padding-right: 10px;
+  padding-left:  10px;
+}
+
+.container {
+  margin-right: auto; 
+  margin-left:  auto; 
+
+  max-width: 960px; 
+
+  padding-right: 10px;
+  padding-left:  10px;
+}
