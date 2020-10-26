dbe84b0bd302993723cb069dc6aa76ea38dccfe1
yyemane32 <yohanayemane57@gmail.com>
2020-10-25 22:41:51 -0400

    1

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
+import countries from"./public/lab_6/countries.js"
 
 dotenv.config();
 
@@ -27,7 +28,7 @@ app.route('/api')
 
   .post((req, res) => {
     console.log('POST request detected');
-    res.send('hello world');
+    res.json(countries);
   });
 
 app.listen(port, () => {
